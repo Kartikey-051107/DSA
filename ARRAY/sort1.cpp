67084feb69a9e86e978449378dc5cd4cc5610dec
Kartikey-051107 <Kartikey05gupta@gmail.com>
2026-02-20 02:23:52 +0530

    sorting

--- ARRAY/sort1.cpp
+++ ARRAY/sort1.cpp
@@ -5,7 +5,9 @@ using namespace std;
 
 int main() 
 {
-    long n;
+    long n,t;
+    cin>>t;
+    for(int p=1;p<=t;p++){
     cin>>n;
    
 
@@ -16,7 +18,7 @@ int main()
 
     }
     for(int i=0;i<n;i++){
-        for(int j=1;j<=n;j++){
+        for(int j=1;j<n;j++){
             int temp;
 
             if(arr[j]<arr[j-1]){
@@ -29,7 +31,8 @@ int main()
     for(int k=0;k<n;k++){
         cout<<arr[k]<<" ";
 
-    }
+
+    } cout<<endl;}
 
 
 }
