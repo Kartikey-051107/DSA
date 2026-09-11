201c2d30e467a67ca26416812ebd20cec495b37b
Kartikey-051107 <Kartikey05gupta@gmail.com>
2026-09-11 15:43:11 +0530

    prints sum of even index numbeer in the given querry

---
+++ prefix_sum.cpp/even_sum_querry.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    long long n;
+    cin>>n;
+    long long  arr[n];
+    for(long long i=0;i<n;i++){
+        cin>>arr[i];
+
+    }
+    for(long long  i=0;i<n;i+=2){
+        arr[i]=0;
+
+    }
+    long long arr2[n];
+    long long sum=0;
+    for(long long i=0;i<n;i++){
+        sum+=arr[i];
+        arr2[i]=sum;
+
+
+
+    }
+    long long q;
+    cin>>q;
+    while(q>0){ 
+    long long l;
+    long long r;
+    cin>>l>>r;
+    l--;
+    r--;
+
+
+    
+    if(l==0){
+        cout<<arr2[r]<<endl;
+
+    }
+    else{
+        cout<<arr2[r]-arr2[(l-1)]<<endl;
+
+    }
+    q--;
+
+}
+   
+}
